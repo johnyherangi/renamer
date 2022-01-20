90aa9a2cbfadfe5204fed22671aa8d59e3b0db82
Johny Herangi <johnyherangi@gmail.com>
2022-01-20 23:33:51 +1300

    WIP

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# node-cli-template
+# Renamer
 
 ## Actions
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,51 @@
 #!/usr/bin/env node
 
-import { helloWorld } from "./index"
+import fs from "fs"
+import { hideBin } from "yargs/helpers"
+import yargs from "yargs/yargs"
 
-console.log(helloWorld())
+yargs(hideBin(process.argv))
+    .scriptName("renamer")
+    .usage("$0 <cmd> [args]")
+    .example([
+        ['$0 run --match "Season \\d Episode \\d .+" --replace "TV Show - S{1}E{2} - {3}"', ""],
+        [
+            '$0 run --recursive --match "Season \\d Episode \\d .+" --replace "TV Show - S{1}E{2} - {3}"',
+            "Rename files in subfolders",
+        ],
+    ])
+    .command(
+        "run [--recursive] <regex> <output>",
+        "Rename files in bulk",
+        {
+            recursive: {
+                type: "boolean",
+                describe: "Rename files in sub folders?",
+            },
+            match: {
+                type: "string",
+                describe: "Match file names",
+                demandOption: true,
+            },
+            replace: {
+                type: "string",
+                describe: "The file's new name",
+            },
+        },
+        (argv) => {
+            const { recursive, match, replace } = argv
+
+            if (!recursive) {
+                const files = fs.readdirSync(process.cwd())
+                console.debug(files)
+            }
+
+            // const getFiles = (path: string) => {
+            //     const files = fs.readdirSync(path)
+
+            //     for ()
+            // }
+        },
+    )
+    .demandOption(["match", "replace"])
+    .parse()
